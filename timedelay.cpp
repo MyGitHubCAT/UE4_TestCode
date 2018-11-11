bfaf947b5ec5d02210bf934669ed29630b0060cb
MyGitHubCAT <44915542+MyGitHubCAT@users.noreply.github.com>
2018-11-11 16:21:03 +0800

    Create timedelay.cpp

---
+++ timedelay.cpp
@@ -0,0 +1,17 @@
+void Classname::Functionname()
+{
+	// try and fire a projectile
+	if (bCanfire)//判定是否进行
+	{
+		bCanfire = false;//更改延迟条件
+		
+		????//添加延时内任意功能
+
+		GetWorld()->GetTimerManager().SetTimer(FireDelayTimerHandle, this, &Afirstperson_codeCharacter::ResetFire, 3.0f, false);//计时3.0f(3s)并运行重置函数
+	}
+}
+void Classname::ResetFire()
+{
+	bCanfire = true;//更改延迟条件
+	GetWorldTimerManager().ClearTimer(FireDelayTimerHandle);//重置计时器
+}
