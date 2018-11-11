1a9546a1a5b61cfc2d7c94152e677a48f8f5cda3
MyGitHubCAT <44915542+MyGitHubCAT@users.noreply.github.com>
2018-11-11 14:45:39 +0800

    pickup.h

---
+++ pickup.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Classes/Components/ShapeComponent.h"
+#include "Classes/Components/BoxComponent.h"
+#include "Components/PrimitiveComponent.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class FIRSTPERSON_CODE_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(EditAnywhere)//定义组件
+	USceneComponent* PickupRoot;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MyMesh;//创建固体网格
+	
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* PickupBox; //碰撞盒子（拾取判定）
+
+	UFUNCTION()//定义功能函数
+	void OnPlayerEnterPickupBox(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);//自定义碰撞函数,注意必须与OnComponentBeginOverlap.AddDynamic中提供的类型一致
+};
