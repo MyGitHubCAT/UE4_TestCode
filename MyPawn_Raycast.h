c21d5ae4db1ed05b194a007758586960560d9715
MyGitHubCAT <44915542+MyGitHubCAT@users.noreply.github.com>
2018-11-13 16:42:09 +0800

    Create MyPawn_Raycast.h

---
+++ MyPawn_Raycast.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "MyPawn_Raycast.generated.h"
+
+UCLASS()
+class FIRSTPERSON_CODE_API AMyPawn_Raycast : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AMyPawn_Raycast();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;//添加映射声明！！！！
+
+	void MyRatcast();//射线声明
+	
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MyMesh;//增加一个mesh组件
+	
+};
